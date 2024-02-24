b0243683236759a509091860ee4329919232b2b8
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-02-24 17:46:26 +0100

    Resources component
    * Flyttet ressurser.js inn i /assets
    * Tar i mot resources (resources er i ressurser.js)

---
+++ src/components/Resources.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Resources({resources}) {
+  const { slug } = useParams();
+  const [resource, setResource] = useState();
+
+  useEffect(() => {
+    console.log(resources)
+    if (resources) {
+      const filteredResource = resources.filter(res => res.category.toLowerCase() === slug.toLowerCase());
+      setResource(filteredResource[0]); // Set to empty object if not found
+    }
+  }, [resources]); // [resources] sier at useEffect skal kjøre på nytt hvis denne forandres (f.eks at innholdet oppdateres)
+  console.log(resource)
+  return (
+    <section>
+          <h2>{resource?.category}</h2>
+          <p>{resource?.text}</p>
+          <ul>
+            {resource?.sources?.map((source, index) => (
+              <li key={index}>
+                <a href={source.url}>{source.title}</a>
+              </li>
+            ))}
+          </ul>
+    </section>
+  );
+}
