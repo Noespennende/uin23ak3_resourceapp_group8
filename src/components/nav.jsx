7b86fc594a0d336f29658a9220abb4f8d5e97e08
Andreasberg89 <andber9@gmail.com>
2024-03-09 18:40:16 +0100

    Nav bruker nå Resources.js

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,17 @@
 import { Link, useParams} from 'react-router-dom';
 import { useState } from 'react'
+import { resources } from '../assets/ressurser';
+
 
 export default function Nav(){
     const {slug} = useParams()
     const [active, setActive] = useState()
-    const categories = ["HTML", "CSS", "JavaScript", "React", "Sanity and headless CMS"]
     console.log("current slug: ", slug)
     return(  
         <>
         <nav>
             <ul>
-                {categories.map((item, index) => <li key={index+"category"}><Link to={"/"+item.replaceAll(" ", "-")} className={active === item? "active" : null}>{item}</Link></li>)}
+                {resources.map((item, index) => <li key={index+"category"}><Link to={"/"+item.category.replaceAll(" ", "-")} className={active === item? "active" : null}>{item.category}</Link></li>)}
             </ul>
         </nav>
         </>
